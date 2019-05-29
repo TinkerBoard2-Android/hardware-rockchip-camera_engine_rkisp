aa66b6de7f05df86a016de79e483eb794423db7f
ZhongYichong <zyc@rock-chips.com>
2019-05-29 11:01:33 +0800

    support multiple flashs for one sensor
    note:
        camera HAL3 should be updated along with this change
    
    Change-Id: I159fa0254c8e23e4d3567b50ed107cf928be5889
    Signed-off-by: ZhongYichong <zyc@rock-chips.com>

--- interface/rkisp_control_loop.h
+++ interface/rkisp_control_loop.h
@@ -27,6 +27,7 @@ using ::android::hardware::camera::common::V1_0::helper::CameraMetadata;
 extern "C" {
 #endif
 
+#define RKISP_SENSOR_ATTACHED_FLASH_MAX_NUM 2
 /*
  * This file is exposed by rkisp control loop(using rkisp CL instead below) library.
  * rkisp CL is the interfaces aggregation to control the ISP, sensor, lens,
@@ -73,7 +74,7 @@ struct rkisp_cl_prepare_params_s {
   // lens subdev node path
   const char* lens_sd_node_path;
   // flashlight subdev node path
-  const char* flashlight_sd_node_path;
+  const char* flashlight_sd_node_path[RKISP_SENSOR_ATTACHED_FLASH_MAX_NUM];
   // static metadata
   const camera_metadata_t *staticMeta;
   // TODO: sensor mode descriptor and others
