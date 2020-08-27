1436f159219040c59bac5ba8bb28ebc30e5be36c
Wang Panzhenzhuan <randy.wang@rock-chips.com>
2020-08-27 11:47:19 +0800

    Android.mk: distinguish isp2.0 & before
    Signed-off-by: Wang Panzhenzhuan <randy.wang@rock-chips.com>
    Change-Id: Icae0d43be9e63bbc961aa27ed8ddaaa63b50fc4f

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(filter rk3368 rk3399 rk3288 rk3366 rk3126c rk3328 rk3326 rk3128h rk322x rk3399pro, $(strip $(TARGET_BOARD_PLATFORM))), )
 CONFIG_FILE_ANDROID = $(shell pwd)/productConfigs.mk
 CONFIG_FILE_LINUX = $(call my-dir)/productConfigs.mk
 ifeq ($(CONFIG_FILE_ANDROID), $(wildcard $(CONFIG_FILE_ANDROID)))
@@ -10,4 +11,8 @@ ifeq ($(IS_ANDROID_OS),true)
 include $(call all-subdir-makefiles)
 else
 include $(call allSubdirMakefiles)
+endif
+else
+#isp2.0 could not compile this
+$(info TARGET_BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM))
 endif
